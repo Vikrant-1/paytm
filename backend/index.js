cbb3f8179ec4c17a294dedf97afa80b928b7a2ed
Vikrant-1 <vikrantkainth090699@gmail.com>
2024-08-11 20:21:42 +0530

    create user routes

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,7 @@
 const express = require("express");
-
+const router = express.Router();
 const app = express();
+const mainRoute = require('./routes/index');
+
+// route import
+router.use('/api/v1', mainRoute);
