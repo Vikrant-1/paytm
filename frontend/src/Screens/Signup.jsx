ccc6abdebbf069f193220f0953bb19305de928bb
Vikrant-1 <vikrantkainth090699@gmail.com>
2024-08-15 16:43:45 +0530

    feat: Add signup form with input fields for first name, last name, email, and password

--- frontend/src/Screens/Signup.jsx
+++ frontend/src/Screens/Signup.jsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+import { TextInput } from "../components/TextInput";
+
 function SignUp() {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="bg-red">
+      <div className="">
+        <h1>Sign Up</h1>
+        <p>Enter your information to create a account</p>
+      </div>
+      <form>
+        <TextInput
+          value={firstName}
+          onValueChange={setFirstName}
+          placeHolder={"Enter First Name"}
+          title={"First Name"}
+        />
+        <TextInput
+          value={lastName}
+          onValueChange={setLastName}
+          placeHolder={"Enter Last Name"}
+          title={"Last Name"}
+        />
+        <TextInput
+          value={email}
+          onValueChange={setEmail}
+          placeHolder={"Enter Email"}
+          title={"Email"}
+        />
+        <TextInput
+          value={password}
+          onValueChange={setPassword}
+          placeHolder={"Enter Password"}
+          title={"Password"}
+        />
+      </form>
     </div>
   );
 }
