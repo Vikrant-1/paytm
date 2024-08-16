00e9914ce6eb1923bcaae6b7a0580c1884876f0e
Vikrant-1 <vikrantkainth090699@gmail.com>
2024-08-16 08:24:19 +0530

    chore: Refactor App component and BalanceComponent to use props for user authentication and balance value

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,21 @@ import Dashboard from "./pages/DashboardPage";
 import SignUp from "./pages/Signup";
 import SignIn from "./pages/Signin";
 import SendMoney from "./pages/Send";
+import { useEffect, useState } from "react";
 function App() {
+  const [isUser, setIsUser] = useState("");
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsUser(!!token);
+  }, []);
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} />
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/components/BalanceComponent.jsx
+++ frontend/src/components/BalanceComponent.jsx
@@ -1,9 +1,9 @@
-export function BalanceComponent() {
+export function BalanceComponent({value}) {
   return (
     <div className="flex w-full justify-center">
       <div className="flex shadow w-11/12 mt-4 rounded py-4 text-lg">
         <div className="font-bold pl-10">Your balance</div>
-        <div className="font-semibold ml-3">Rs 10,000</div>
+        <div className="font-semibold ml-3">{value}</div>
       </div>
     </div>
   );
